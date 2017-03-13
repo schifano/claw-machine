5684edcadcbc0ae57beb0f79a2bd018a97f48112
Rachel Schifano <rschifa1@gmail.com>
2017-03-12 23:14:55 -0500

    Add open boundary view for mouth

--- ClawMachine.playground/Contents.swift
+++ ClawMachine.playground/Contents.swift
@@ -5,7 +5,7 @@ import PlaygroundSupport
 
 let container = Container()
 container.setup()
-container.getContainerSize()
+//container.getContainerSize()
 
 // TODO: Generate multiple items on screen
 // TODO: Add overlap behavior
@@ -15,7 +15,7 @@ container.getContainerSize()
 // UIKit Dynamics - drop things
 //let containerView = container.viewController.view!
 
-let containerView = container.container
+let containerView = container.clawMachineContainer
 
 let square = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
 square.backgroundColor = UIColor.purple
@@ -24,13 +24,13 @@ let stuffedAnimalNames = ["bear.jpg", "unicorn.jpg", "duck.jpg"]
 let images = stuffedAnimalNames.map { UIImage(named: $0) }
 
 let bear = UIImageView(image: images[0])
-bear.frame = CGRect(x: 100, y: 100, width: 80, height: 80)
+bear.frame = CGRect(x: 100, y: 100, width: 60, height: 60)
 
 let ooni = UIImageView(image: images[1])
-ooni.frame = CGRect(x: 105, y: 100, width: 80, height: 80)
+ooni.frame = CGRect(x: 105, y: 100, width: 60, height: 60)
 
 let quacky = UIImageView(image: images[2])
-quacky.frame = CGRect(x: 110, y: 100, width: 80, height: 80)
+quacky.frame = CGRect(x: 110, y: 100, width: 60, height: 60)
 
 containerView.addSubview(bear)
 containerView.addSubview(ooni)
@@ -50,13 +50,16 @@ animator.addBehavior(gravity)
 
 
 // claw machine mouth barrier
-let barrierMouthLeft = UIView(frame: CGRect(x: 60, y: 300, width: 5, height: 100))
+let barrierMouthLeft = UIView(frame: CGRect(x: 50, y: 300, width: 5, height: 100))
 barrierMouthLeft.backgroundColor = UIColor.gray
 
 containerView.addSubview(barrierMouthLeft)
-let barrierMouthRight = UIView(frame: CGRect(x: 200, y: 300, width: 5, height: 100))
+let barrierMouthRight = UIView(frame: CGRect(x: 150, y: 300, width: 5, height: 100))
 barrierMouthRight.backgroundColor = UIColor.gray
 containerView.addSubview(barrierMouthRight)
+
+barrierMouthLeft.autoresizingMask = [.flexibleLeftMargin, .flexibleRightMargin] // adjusts only on iPad
+
 
 collision = UICollisionBehavior(items: dynamicItems)
 // translates uses the bounds of the reference view, aka container
@@ -66,6 +69,6 @@ animator.addBehavior(collision)
 collision.addBoundary(withIdentifier: "barrierMouthLeft" as NSCopying, for: UIBezierPath(rect: barrierMouthLeft.frame))
 collision.addBoundary(withIdentifier: "barrierMouthright" as NSCopying, for: UIBezierPath(rect: barrierMouthRight.frame))
 
-let window = container.gameWindow
+let window = container.gameWindowRight
 collision.addBoundary(withIdentifier: "window" as NSCopying, for: UIBezierPath(rect: window.frame))
 
--- ClawMachine.playground/Sources/Container.swift
+++ ClawMachine.playground/Sources/Container.swift
@@ -3,48 +3,44 @@ import PlaygroundSupport
 
 public class Container {
 
-    public let viewController = UIViewController()
+//    public let container = UIViewController()
     
-    public let container = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 800))
+    public let clawMachineContainer = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 700))
     public let gameWindow = UIView(frame: CGRect(x: 20, y: 90, width: 460, height: 300))
+    
+    public let gameWindowLeft = UIView(frame: CGRect(x: 20, y: 90, width: 150, height: 300))
+    public let gameWindowRight = UIView(frame: CGRect(x: 170, y: 90, width: 310, height: 300))
+    
+    
+//    public let gameWindow = UIView(frame: CGRect(x: 20, y: 90, width: 392, height: 300))    // iPad
     
     public init() { } // needs to be public to be accessible from main playground
     
     public func setup() {
-//        viewController.view.layer.backgroundColor = UIColor.green.cgColor
-//        viewController.view.frame = CGRect(x: 0, y: 0, width: 500, height: 700)
-        
-//        let container = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 700))
-        
-        container.backgroundColor = UIColor.green
+        clawMachineContainer.backgroundColor = UIColor.green
         gameWindow.backgroundColor = UIColor.cyan
         
-        container.addSubview(gameWindow)
+        gameWindowLeft.backgroundColor = UIColor.black
+        gameWindowRight.backgroundColor = UIColor.brown
         
         
-        PlaygroundPage.current.liveView = container
+        // Set autoreizingMask for compatibility in iPad Playgrounds
+        // This adjusts only the gameWindow, other views need adjusting - try bottom eventually
+        // 432 is the min root view width for iPad
+        gameWindow.autoresizingMask = [.flexibleLeftMargin, .flexibleRightMargin]
+        
+//        clawMachineContainer.addSubview(gameWindow)
+        
+        clawMachineContainer.addSubview(gameWindowLeft)
+        clawMachineContainer.addSubview(gameWindowRight)
+        
+        PlaygroundPage.current.liveView = clawMachineContainer
         PlaygroundPage.current.needsIndefiniteExecution = true
-        
-//        let container = UIView(frame: CGRect(x: 0, y: 0, width: 600, height: 800))
-//        container.backgroundColor = UIColor.white
-//        
-//        let view = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-//        view.backgroundColor = UIColor.red
-//        container.addSubview(view)
-//
-//        PlaygroundPage.current.liveView = container
-//        PlaygroundPage.current.needsIndefiniteExecution = true
-//        ViewController Height: 668.0
-//        ViewController Width: 375.0
-
     }
 
     public func getContainerSize() {
         // DEBUG
-//        print("ViewController Height: \(viewController.view.frame.size.height)")
-//        print("ViewController Width: \(viewController.view.frame.size.width)")
-        
-        print("Container Width: \(container.frame.size.width)")
-        print("Container Height: \(container.frame.size.height)")
+        print("Container Width: \(clawMachineContainer.frame.size.width)")
+        print("Container Height: \(clawMachineContainer.frame.size.height)")
     }
 }
