9f4e423a876e1e9bca63fae32aed533a4501227c
Rachel Schifano <rschifa1@gmail.com>
2017-03-12 19:42:37 -0500

    Add collision barriers to simulate mouth of machine

--- ClawMachine.playground/Contents.swift
+++ ClawMachine.playground/Contents.swift
@@ -8,13 +8,12 @@ container.setup()
 //container.getContainerSize()
 
 // TODO: Generate multiple items on screen
-// TODO: Physics drop items
-
+// TODO: Add overlap behavior
 
 
 
 // UIKit Dynamics - drop things
-let containerView = container.viewController.view
+let containerView = container.viewController.view!
 
 let square = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
 square.backgroundColor = UIColor.purple
@@ -23,17 +22,17 @@ let stuffedAnimalNames = ["bear.jpg", "unicorn.jpg", "duck.jpg"]
 let images = stuffedAnimalNames.map { UIImage(named: $0) }
 
 let bear = UIImageView(image: images[0])
-bear.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+bear.frame = CGRect(x: 100, y: 100, width: 80, height: 80)
 
 let ooni = UIImageView(image: images[1])
-ooni.frame = CGRect(x: 180, y: 120, width: 100, height: 100)
+ooni.frame = CGRect(x: 105, y: 100, width: 80, height: 80)
 
 let quacky = UIImageView(image: images[2])
-quacky.frame = CGRect(x: 200, y: 105, width: 100, height: 100)
+quacky.frame = CGRect(x: 110, y: 100, width: 80, height: 80)
 
-containerView?.addSubview(bear)
-containerView?.addSubview(ooni)
-containerView?.addSubview(quacky)
+containerView.addSubview(bear)
+containerView.addSubview(ooni)
+containerView.addSubview(quacky)
 
 // FIXME: Can I make these optional
 var animator: UIDynamicAnimator!
@@ -42,11 +41,26 @@ var collision: UICollisionBehavior!
 
 let dynamicItems = [bear, ooni, quacky]
 
-animator = UIDynamicAnimator(referenceView: containerView!)
+animator = UIDynamicAnimator(referenceView: containerView)
 gravity = UIGravityBehavior(items: dynamicItems)
 animator.addBehavior(gravity)
+
+
+
+// claw machine mouth barrier
+let barrierMouthLeft = UIView(frame: CGRect(x: 100, y: 300, width: 5, height: 100))
+barrierMouthLeft.backgroundColor = UIColor.gray
+
+containerView.addSubview(barrierMouthLeft)
+let barrierMouthRight = UIView(frame: CGRect(x: 240, y: 300, width: 5, height: 100))
+barrierMouthRight.backgroundColor = UIColor.gray
+containerView.addSubview(barrierMouthRight)
 
 collision = UICollisionBehavior(items: dynamicItems)
 // translates uses the bounds of the reference view, aka container
 collision.translatesReferenceBoundsIntoBoundary = true
 animator.addBehavior(collision)
+
+collision.addBoundary(withIdentifier: "barrierMouthLeft" as NSCopying, for: UIBezierPath(rect: barrierMouthLeft.frame))
+collision.addBoundary(withIdentifier: "barrierMouthright" as NSCopying, for: UIBezierPath(rect: barrierMouthRight.frame))
+
