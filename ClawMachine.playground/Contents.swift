467c5cee5d428cb25433d5ca3730144fe4b413d5
Rachel Schifano <rschifa1@gmail.com>
2017-03-14 21:42:16 -0500

    Set physics boundary path for machine

--- ClawMachine.playground/Contents.swift
+++ ClawMachine.playground/Contents.swift
@@ -1,6 +1,7 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import SpriteKit
 import PlaygroundSupport
 
 let container = Container()
@@ -15,41 +16,110 @@ container.setup()
 // UIKit Dynamics - drop things
 //let containerView = container.viewController.view!
 
-let containerView = container.clawMachineContainer
+let containerView = container.clawMachineContainerView
 
-let square = UIView(frame: CGRect(x: 100,  y: 100, width: 100, height: 100))
-square.backgroundColor = UIColor.purple
+//let boundary = SKNode()
+let path = CGMutablePath()
+path.addLines(between: [
+    CGPoint(x: 0, y: container.physicsContainerView.frame.height),
+    CGPoint(x: container.physicsContainerView.frame.width, y: container.physicsContainerView.frame.height),
+    CGPoint(x: container.physicsContainerView.frame.width, y: container.physicsContainerView.frame.height - container.gameWindow.frame.height),
+    CGPoint(x: 140, y: container.physicsContainerView.frame.height - container.gameWindow.frame.height),
+    CGPoint(x: 140, y: 0),
+    CGPoint(x: 20, y: 0),
+    CGPoint(x: 20, y: container.physicsContainerView.frame.height - container.gameWindow.frame.height),
+    CGPoint(x: 0, y: container.physicsContainerView.frame.height - container.gameWindow.frame.height)
+//    CGPoint(x: 460, y: 390),
+//    CGPoint(x: 150, y: 300),
+//    CGPoint(x: 20, y: 90)
+    ])
 
-let stuffedAnimalNames = ["bear.jpg", "unicorn.jpg", "duck.jpg"]
-let images = stuffedAnimalNames.map { UIImage(named: $0) }
+//path.addLines(between: [
+//    CGPoint(x: 0, y: 0),
+//    CGPoint(x: 600, y: 500),
+//    ])
+path.closeSubpath()
+//boundary.physicsBody = SKPhysicsBody(polygonFrom: path)
 
-let bear = UIImageView(image: images[0])
-bear.frame = CGRect(x: 100, y: 100, width: 60, height: 60)
+// y: containerHeight - gameWindowHeight, coords start bottom left
+//let boundary = SKNode()
+//boundary.physicsBody = SKPhysicsBody(edgeLoopFrom:
+//    CGRect(x: 0, y: 250,
+//           width: container.gameWindow.frame.width,
+//           height: container.gameWindow.frame.height))
+//container.scene.addChild(boundary)
 
-let ooni = UIImageView(image: images[1])
-ooni.frame = CGRect(x: 105, y: 100, width: 60, height: 60)
-
-let quacky = UIImageView(image: images[2])
-quacky.frame = CGRect(x: 110, y: 100, width: 60, height: 60)
-
-containerView.addSubview(bear)
-containerView.addSubview(ooni)
-containerView.addSubview(quacky)
-
-// FIXME: Can I make these optional
-var animator: UIDynamicAnimator!
-var gravity: UIGravityBehavior!
-var collision: UICollisionBehavior!
-
-let dynamicItems = [bear, ooni, quacky]
-
-animator = UIDynamicAnimator(referenceView: containerView)
-gravity = UIGravityBehavior(items: dynamicItems)
-animator.addBehavior(gravity)
+let fullBoundary = SKNode()
+fullBoundary.physicsBody = SKPhysicsBody(edgeLoopFrom: path)
+container.scene.addChild(fullBoundary)
 
 
+let texture = SKTexture(image: #imageLiteral(resourceName: "unicorn.jpg"))
 
-// claw machine mouth barrier
+for _ in 1 ... 10 {
+    let ooni = SKSpriteNode(texture: texture)
+    ooni.size = CGSize(width: 50, height: 50)
+    ooni.position = CGPoint(x: Int(arc4random_uniform(141)+200), y: Int(arc4random_uniform(251) + 250))
+    ooni.physicsBody = SKPhysicsBody(texture: texture, size: CGSize(width: 30, height: 30))
+    ooni.physicsBody?.affectedByGravity = true
+    container.scene.addChild(ooni)
+}
+//
+//let texture = SKTexture(image: #imageLiteral(resourceName: "bear2.png"))
+//for _ in 1 ... 25 {
+//    let star = SKSpriteNode(texture: texture)
+//    star.size = CGSize(width: 50, height: 50)
+//    //    star.size = CGSize(width: 10, height: 10)
+//    
+//    star.position = CGPoint(x: Int(arc4random() %  150), y: Int(arc4random() % 200))
+//    
+//    
+//    star.physicsBody = SKPhysicsBody(texture: texture, size: CGSize(width: 6.5, height: 6.5))
+//    scene.addChild(star)
+//
+
+
+
+
+
+
+
+
+//
+//let square = UIView(frame: CGRect(x: 100,  y: 100, width: 100, height: 100))
+//square.backgroundColor = UIColor.purple
+//
+//let stuffedAnimalNames = ["bear.jpg", "unicorn.jpg", "duck.jpg"]
+//let images = stuffedAnimalNames.map { UIImage(named: $0) }
+//
+//let bear = UIImageView(image: images[0])
+//bear.frame = CGRect(x: 100, y: 100, width: 60, height: 60)
+//
+//let ooni = UIImageView(image: images[1])
+//ooni.frame = CGRect(x: 105, y: 100, width: 60, height: 60)
+//
+//let quacky = UIImageView(image: images[2])
+//quacky.frame = CGRect(x: 110, y: 100, width: 60, height: 60)
+//
+//
+//containerView.addSubview(bear)
+//containerView.addSubview(ooni)
+//containerView.addSubview(quacky)
+//
+//// FIXME: Can I make these optional
+//var animator: UIDynamicAnimator!
+//var gravity: UIGravityBehavior!
+//var collision: UICollisionBehavior!
+//
+//let dynamicItems = [bear, ooni, quacky]
+//
+//animator = UIDynamicAnimator(referenceView: containerView)
+//gravity = UIGravityBehavior(items: dynamicItems)
+//animator.addBehavior(gravity)
+//
+//
+//
+//// claw machine mouth barrier
 let barrierMouthLeft = UIView(frame: CGRect(x: 50, y: 300, width: 5, height: 100))
 barrierMouthLeft.backgroundColor = UIColor.gray
 
@@ -58,17 +128,17 @@ let barrierMouthRight = UIView(frame: CGRect(x: 150, y: 300, width: 5, height: 1
 barrierMouthRight.backgroundColor = UIColor.gray
 containerView.addSubview(barrierMouthRight)
 
-barrierMouthLeft.autoresizingMask = [.flexibleLeftMargin, .flexibleRightMargin] // adjusts only on iPad
-
-
-collision = UICollisionBehavior(items: dynamicItems)
-// translates uses the bounds of the reference view, aka container
-collision.translatesReferenceBoundsIntoBoundary = true
-animator.addBehavior(collision)
-
-collision.addBoundary(withIdentifier: "barrierMouthLeft" as NSCopying, for: UIBezierPath(rect: barrierMouthLeft.frame))
-collision.addBoundary(withIdentifier: "barrierMouthright" as NSCopying, for: UIBezierPath(rect: barrierMouthRight.frame))
-
-let window = container.gameWindowRight
-collision.addBoundary(withIdentifier: "window" as NSCopying, for: UIBezierPath(rect: window.frame))
-
+//barrierMouthLeft.autoresizingMask = [.flexibleLeftMargin, .flexibleRightMargin] // adjusts only on iPad
+//
+//
+//collision = UICollisionBehavior(items: dynamicItems)
+//// translates uses the bounds of the reference view, aka container
+//collision.translatesReferenceBoundsIntoBoundary = true
+//animator.addBehavior(collision)
+//
+//collision.addBoundary(withIdentifier: "barrierMouthLeft" as NSCopying, for: UIBezierPath(rect: barrierMouthLeft.frame))
+//collision.addBoundary(withIdentifier: "barrierMouthright" as NSCopying, for: UIBezierPath(rect: barrierMouthRight.frame))
+//
+////let window = container.gameWindowRight
+////collision.addBoundary(withIdentifier: "window" as NSCopying, for: UIBezierPath(rect: window.frame))
+//
--- ClawMachine.playground/Sources/Container.swift
+++ ClawMachine.playground/Sources/Container.swift
@@ -1,46 +1,65 @@
 import UIKit
+import SpriteKit
 import PlaygroundSupport
 
 public class Container {
 
-//    public let container = UIViewController()
+    public let clawMachineContainerView = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 700))
     
-    public let clawMachineContainer = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 700))
+    public let physicsContainerView = SKView(frame: CGRect(x: 20, y: 90, width: 460, height: 550))
+    public let scene = SKScene(size: CGSize(width: 460, height: 550))
+    
+    
+//    public let clawMachineContainer = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 700))
     public let gameWindow = UIView(frame: CGRect(x: 20, y: 90, width: 460, height: 300))
-    
-    public let gameWindowLeft = UIView(frame: CGRect(x: 20, y: 90, width: 150, height: 300))
-    public let gameWindowRight = UIView(frame: CGRect(x: 170, y: 90, width: 310, height: 300))
-    
+//
+//    public let gameWindowLeft = UIView(frame: CGRect(x: 20, y: 90, width: 150, height: 300))
+//    public let gameWindowRight = UIView(frame: CGRect(x: 170, y: 90, width: 310, height: 300))
+//    
     
 //    public let gameWindow = UIView(frame: CGRect(x: 20, y: 90, width: 392, height: 300))    // iPad
     
     public init() { } // needs to be public to be accessible from main playground
     
     public func setup() {
-        clawMachineContainer.backgroundColor = UIColor.green
-        gameWindow.backgroundColor = UIColor.cyan
-        
-        gameWindowLeft.backgroundColor = UIColor.darkGray
-        gameWindowRight.backgroundColor = UIColor.brown
-        
+
+//
+//        gameWindowLeft.backgroundColor = UIColor.darkGray
+//        gameWindowRight.backgroundColor = UIColor.brown
+//        
         
         // Set autoreizingMask for compatibility in iPad Playgrounds
         // This adjusts only the gameWindow, other views need adjusting - try bottom eventually
         // 432 is the min root view width for iPad
+        
+        clawMachineContainerView.backgroundColor = UIColor(red: 127/255, green: 0/255, blue: 255/255, alpha: 1.0)
+        
+        gameWindow.backgroundColor = UIColor.cyan
         gameWindow.autoresizingMask = [.flexibleLeftMargin, .flexibleRightMargin]
         
 //        clawMachineContainer.addSubview(gameWindow)
         
-        clawMachineContainer.addSubview(gameWindowLeft)
-        clawMachineContainer.addSubview(gameWindowRight)
+//        clawMachineContainer.addSubview(gameWindowLeft)
+//        clawMachineContainer.addSubview(gameWindowRight)
+//        PlaygroundPage.current.liveView = clawMachineContainer
         
-        PlaygroundPage.current.liveView = clawMachineContainer
+        physicsContainerView.backgroundColor = UIColor(red: 255/255, green: 0/255, blue: 128/255, alpha: 1.0)
+        scene.scaleMode = SKSceneScaleMode.aspectFit
+        scene.backgroundColor = UIColor(red: 255/255, green: 0/255, blue: 128/255, alpha: 1.0)
+        physicsContainerView.presentScene(scene)
+        
+        physicsContainerView.showsPhysics = true
+        physicsContainerView.showsFields = true
+        
+        clawMachineContainerView.addSubview(physicsContainerView)
+        
+        PlaygroundPage.current.liveView = clawMachineContainerView
         PlaygroundPage.current.needsIndefiniteExecution = true
     }
 
-    public func getContainerSize() {
-        // DEBUG
-        print("Container Width: \(clawMachineContainer.frame.size.width)")
-        print("Container Height: \(clawMachineContainer.frame.size.height)")
-    }
+//    public func getContainerSize() {
+//        // DEBUG
+//        print("Container Width: \(clawMachineContainer.frame.size.width)")
+//        print("Container Height: \(clawMachineContainer.frame.size.height)")
+//    }
 }
