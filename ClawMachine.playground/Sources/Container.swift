18d7ecc2f6417eb20e875a5e24de8a1f278b0989
Rachel Schifano <rschifa1@gmail.com>
2017-03-23 20:23:47 -0500

    Create SKShapes instead of views and redraw boundaries relative to shapes

--- ClawMachine.playground/Sources/Container.swift
+++ ClawMachine.playground/Sources/Container.swift
@@ -2,137 +2,112 @@ import UIKit
 import SpriteKit
 import PlaygroundSupport
 
+// TODO: Add gamePanel
+// TODO: consider making a blurry transparent background for window effect
+
 public class Container {
-
-    /// Main view of the claw machine that all other views are added to
-    /// width 432 max is used for iPad Playgrounds
-//    public let clawMachineCabinetContainerView = UIView(frame: CGRect(x: 0, y: 0, width: 432, height: 600))
     
+    struct Colors {
+        static let cyan = color(for: 0x98E3D4)  // soft cyan
+        static let magenta = color(for: 0xd498e3)   // soft magenta
+        static let gray = UIColor.gray
+        
+        /// Precede each hex int value with 0x
+        static func color(for hex: Int) -> UIColor {
+            return UIColor(red: CGFloat(Float((hex & 0xff0000) >> 16) / 255.0),
+                           green: CGFloat(Float((hex & 0x00ff00) >> 8) / 255.0),
+                           blue: CGFloat(Float((hex & 0x0000ff) >> 0) / 255.0),
+                           alpha: CGFloat(1.0))
+        }
+    }
     
-    public let clawMachineCabinetContainerView = SKView(frame: CGRect(x: 0, y: 0, width: 432, height: 600))
+    static let cabinetWidth = 432  /// width 432 max is used for iPad Playgrounds
+    static let cabinetHeight = 580
+    static let boundaryWidth = 392
+    static let boundaryHeight = 450
     
-//    /// View where all physics occurs
-    public let physicsContainerView = SKView(frame: CGRect(x: 20, y: 90, width: 392, height: 450))
-    public let scene = SKScene(size: CGSize(width: 392, height: 450))
+    static let clawMachineCabinetContainerView = SKView(frame: CGRect(x: 0, y: 0, width: cabinetWidth, height: cabinetHeight))
+    static let scene = SKScene(size: CGSize(width: cabinetWidth, height: cabinetHeight))
     
-    public let gameWindow = SKView(frame: CGRect(x: 20, y: 90, width: 392, height: 200))
-    public let gamePanel = UIView(frame: CGRect(x: 120, y: 350, width: 50, height: 50))
-    // TODO: Add gamePanel and dispenserWindow
+    static let gameWindowShape = SKShapeNode(rect: CGRect(x: 20, y: 300, width: boundaryWidth, height: 200))
+    static let prizeShootShape = SKShapeNode(rect: CGRect(x: 40, y: 70, width: 70, height: 310))
+    static let prizeDispenser = SKShapeNode(rect: CGRect(x: prizeShootShape.frame.minX, y: prizeShootShape.frame.minY, width: prizeShootShape.frame.width, height: prizeShootShape.frame.width))
+    static let button = Button.init(defaultButtonImage: "button-default.png", activeButtonImage: "button-active.png", buttonAction: buttonIsPressed)
     
-    /// Colors
-//    let cabinetColor = UIColor(red: 152/255, green: 227/255, blue: 212/255, alpha: 1.0) // pale robin egg blue: #98E3D4
-    let cabinetColor = UIColor(red:0.39, green:0.00, blue:0.76, alpha:1.00)
-//    let gameWindowColor = UIColor(red: 212/255, green: 152/255, blue: 227/255, alpha: 1.0) // soft magenta #d498e3
-    let gameWindowColor = UIColor(red: 152/255, green: 227/255, blue: 212/255, alpha: 1.0)
-    // TODO: consider making a blurry transparent background for window effect
-    let gamePanelColor = UIColor(red: 152/255, green: 227/255, blue: 212/255, alpha: 1.0)
-    
-    
-    
-    public let crane = Crane.init(defaultCraneImage: "crane.png")
-    
-    public init() { } // needs to be public to be accessible from main playground
-    
-    public func setup() {
-
-        // FIXME: lol this doesn't work
-        clawMachineCabinetContainerView.contentMode = .scaleAspectFit
+    public static func setup() {
+        clawMachineCabinetContainerView.backgroundColor = Colors.gray
+        clawMachineCabinetContainerView.showsFields = true
+        clawMachineCabinetContainerView.showsPhysics = true
         
-        
-        
-        
-        
-        // FIXME: Adjust views to be resizable on iPad
-        // 432 is the min root view width for iPad
-        
-        // TODO: CREATE WORKING BUTTONS
-        let button = Button.init(defaultButtonImage: "button-default.png", activeButtonImage: "button-active.png", buttonAction: doSomething)
-        button.position = CGPoint(x: 250, y: 150)
-        
-        
-//        let crane = Crane.init(defaultCraneImage: "crane.png")
-        crane.position = CGPoint(x: 55, y: 410)
-        
-        gameWindow.backgroundColor = gameWindowColor
-        gamePanel.backgroundColor = gamePanelColor
-        clawMachineCabinetContainerView.backgroundColor = cabinetColor
-        
-        gamePanel.layer.cornerRadius = 25
-        gamePanel.layer.masksToBounds = true
-        
-        clawMachineCabinetContainerView.addSubview(gameWindow)
-//        clawMachineCabinetContainerView.addSubview(gamePanel)
-//        clawMachineCabinetContainerView.addSubview(physicsContainerView)
-        
-        
-//        clawMachineCabinetContainerView.addSubview(physicsContainerView)
-        physicsContainerView.allowsTransparency = true
-        physicsContainerView.showsPhysics = true    // debug
-        physicsContainerView.showsFields = true     // debug
-        
-        scene.backgroundColor = UIColor.clear
+        // Scene
+        scene.backgroundColor = Colors.magenta
         scene.scaleMode = SKSceneScaleMode.aspectFit
-        physicsContainerView.contentMode = .scaleAspectFit
+
+        // game window
+        gameWindowShape.fillColor = UIColor.blue
+        scene.addChild(gameWindowShape)
         
-        scene.addChild(button)  // BUTTON
-        scene.addChild(crane)   // CRANE
+        // prize shoot
+        prizeShootShape.fillColor = UIColor.clear
+        prizeShootShape.lineWidth = 0.0
+        scene.addChild(prizeShootShape)
         
-        drawBoundaries()
+        // prize dispenser
+        prizeDispenser.fillColor = UIColor.black
+        prizeDispenser.lineWidth = 0.0
+        scene.addChild(prizeDispenser)
+    
+        // button
+        button.position = CGPoint(x: 270, y: 210)
+        scene.addChild(button)
+        
+        // draw boundaries on scene
+        let boundary = drawBoundaries()
+        scene.addChild(boundary)
         
         clawMachineCabinetContainerView.presentScene(scene)
         
         PlaygroundPage.current.liveView = clawMachineCabinetContainerView
         PlaygroundPage.current.needsIndefiniteExecution = true
-
     }
     
-    // FIXME: Moves crane in designated path
-    func doSomething() {
-        let moveNodeRight = SKAction.moveBy(x: 150.0,
-                                            y: 0.0,
-                                            duration: 2.0)
-        let moveNodeDown = SKAction.moveBy(x: 0.0,
-                                           y: -100.0,
-                                           duration: 1.0)
+    // Method used to draw claw machine boundaries
+    // pass in the frame to get precise on w/e
+    static func drawBoundaries() -> SKNode {
+        let windowMinX = gameWindowShape.frame.minX
+        let windowMaxX = gameWindowShape.frame.maxX
+        let windowMaxY = gameWindowShape.frame.maxY
+        let windowMinY = gameWindowShape.frame.minY
         
-        let sequence = SKAction.sequence([moveNodeRight, moveNodeDown, moveNodeDown.reversed(), moveNodeRight.reversed()])
-        crane.run(sequence)
-    }
-    
-    func drawBoundaries() {
-        let physicsHeight = physicsContainerView.frame.height
-        let physicsWidth = physicsContainerView.frame.width
-        let windowHeight = gameWindow.frame.height
+        let prizeMinX = prizeShootShape.frame.minX
+        let prizeMaxX = prizeShootShape.frame.maxX
+        let prizeMinY = prizeShootShape.frame.minY
+        let prizeMaxY = prizeShootShape.frame.maxY
         
         let path = CGMutablePath()
         path.addLines(between: [
-            CGPoint(x: 0, y: physicsHeight),
-            CGPoint(x: physicsWidth, y: physicsHeight),
-            CGPoint(x: physicsWidth, y: physicsHeight - windowHeight),
-            CGPoint(x: 105, y: physicsHeight - windowHeight),
-            CGPoint(x: 105, y: physicsHeight - windowHeight + 100),
-            CGPoint(x: 100, y: physicsHeight - windowHeight + 100),
-            CGPoint(x: 100, y: 0),
-            CGPoint(x: 15, y: 0),
-            CGPoint(x: 15, y: physicsHeight - windowHeight + 100),
-            CGPoint(x: 10, y: physicsHeight - windowHeight + 100),
-            CGPoint(x: 10, y: physicsHeight - windowHeight),
-            
-            CGPoint(x: 0, y: physicsHeight - windowHeight)
+            CGPoint(x: windowMinX, y: windowMaxY),
+            CGPoint(x: windowMaxX, y: windowMaxY),
+            CGPoint(x: windowMaxX, y: windowMinY),
+            CGPoint(x: prizeMaxX+5, y: windowMinY),
+            CGPoint(x: prizeMaxX+5, y: prizeMaxY),
+            CGPoint(x: prizeMaxX, y: prizeMaxY),
+            CGPoint(x: prizeMaxX, y: prizeMinY),
+            CGPoint(x: prizeMinX, y: prizeMinY),
+            CGPoint(x: prizeMinX, y: prizeMaxY),
+            CGPoint(x: prizeMinX-5, y: prizeMaxY),
+            CGPoint(x: prizeMinX-5, y: windowMinY),
+            CGPoint(x: windowMinX, y: windowMinY)
             ])
         path.closeSubpath()
         
         let fullBoundary = SKNode()
         fullBoundary.physicsBody = SKPhysicsBody(edgeLoopFrom: path)
-        scene.addChild(fullBoundary)
+        
+        return fullBoundary
     }
     
-    
-    
-    
-//    public func getContainerSize() {
-//        // DEBUG
-//        print("Container Width: \(clawMachineContainer.frame.size.width)")
-//        print("Container Height: \(clawMachineContainer.frame.size.height)")
-//    }
+    static func buttonIsPressed() {
+        print("Button pressed")
+    }
 }
