1c1008de1617e5cb6bc53a476c2739d85d517761
Rachel Schifano <rschifa1@gmail.com>
2017-03-31 15:51:47 -0500

    Delete bear

--- ClawMachine.playground/Contents.swift
+++ ClawMachine.playground/Contents.swift
@@ -7,23 +7,20 @@ import PlaygroundSupport
 ClawMachine.setup()
 
 // Create stuffed animal sprites
-//Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "bear3.png"), quantity: 1)
-Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-goldi.png"), quantity: 1)
-//Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-giraffee.png"), quantity: 1)
-//Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-bunny.png"), quantity: 1)
-//Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-bully.png"), quantity: 1)
+//Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-bunny.png"), quantity: 3)
+//Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-giraffee.png"), quantity: 2)
+Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-bully.png"), quantity: 5)
 
-// Experiment with the strength of the claw by adjusting force used to open and close it
+//Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-oinky.png"), quantity: 1)
+Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-beary.png"), quantity: 1)
+
+//Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-goldi.png"), quantity: 1)
+//Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-toffee.png"), quantity: 1)
+
+// Experiment with the strength of the claw
+// Adjust force used to open and close it
 Claw.openStrength = 60
 Claw.closeStrength = 100
 
-// TODO: Customize speed of claw (duration in the repeating action)
-
-
-// TODO: Customize skin of machine
-// TODO: Add name at the top in a font?
-// TODO: Sound of stuffed animal at bottom??
-
-
-//ClawMachine.name = "Rachel"
+// Change shape of claw
 
