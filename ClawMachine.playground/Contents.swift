de951b1c68822e5ddd07b2e94e0ace7f8de99724
Rachel Schifano <rschifa1@gmail.com>
2017-03-31 18:13:05 -0500

    Clean up the content

--- ClawMachine.playground/Contents.swift
+++ ClawMachine.playground/Contents.swift
@@ -6,20 +6,15 @@ import PlaygroundSupport
 
 ClawMachine.setup()
 
+// Hold down the button to move the crane!
+// Release the button to attempt a prize
+
 // Create stuffed animal sprites
-//Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-giraffee.png"), quantity: 2) // Giraffee
-Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-bunny.png"), quantity: 4) // Bunny
-//Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-bully.png"), quantity: 2) // Bully
-
-//Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-oinky.png"), quantity: 1)
-
-
-//Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-goldi.png"), quantity: 1)
-//Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-toffee.png"), quantity: 1)
-
-//Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-beary.png"), quantity: 1) // Beary
+Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-bully.png"), quantity: 1) // Bully
+Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-giraffee.png"), quantity: 2) // Giraffee
+Sprites.createStuffedAnimal(image: #imageLiteral(resourceName: "sprite-bunny.png"), quantity: 3) // Bunny
 
 // Experiment with the strength of the claw
-// Adjust force used to open and close it
+// Adjust force used to open and close
 Claw.openStrength = 60
 Claw.closeStrength = 100
